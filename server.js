9c7cdac26b15c07bd637f4e459c4164e29af3147
Simonni <szkiflay@gmal.com>
2018-12-04 21:32:56 -0500

    lab-6

--- server.js
+++ server.js
@@ -3,39 +3,41 @@ const express = require('express')
 const cors = require('cors')
 const superagent = require('superagent')
 
-
-
-const port = process.env.port ||3000
-
 require('dotenv').config()
+const port = process.env.port ||3000
 const app = express()
 process.env.port || 3000
 
 app.use(cors())
 
 app.get('/', (req, res) =>
-  res.send('listening to the port port localHost'))
+  res.send('<h1>listening to the port Simon localHost</h1>'))
 
 app.get('*', (req, res)=>
-  res.send('500 error message'))
+  res.send('<img src="https://http.cat/404" />'))
 
+app.get('/location', (req, res)=>{
+  const url = `https://maps.googleapis.com/maps/api/geocode/json?address=${req.query.address}&key=${process.env.GOOGLE_API_KEY}`
+  superagent.get(url)
+    .then(result => {
+      res.send(new Location(result))
+      { lng: result.body.results[0].geometry.location.lng
+        lat: result.body.results[0].geometry.location.lat
+      }
+    })
 
+    .catch(err => res.send(err))
+})
+app.get('/', (req, res)=>{
+  res.send('this is home route')
+})
 
-
+app.use('*', (req, res)=>
+  res.send('Something broke'))
 app.listen(port, ()=>{
   console.log(`Listening to port ${port}`)
 })
-
-app.get('/location', (req, res)=>{
-  const url = `https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=${process.env.GOOGLE_API_KEY}`
-  superagent.get(url)
-    .then(result => {
-      res.send({
-        longitude: result.body.results[0].geometry.location.lng,
-        latitude: result.body.results[0].geometry.location.lat
-      })
-    })
-    .catch(err => res.send('Got an error'))
-})
- 
-
+const Location = function(loc){
+  this.lat = loc.body.results[0].geometry.location.lat
+  this.lng = loc.body.results[0].geometry.location.lng
+}
