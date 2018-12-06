3495035a8ecf21cc08ab2ad3030dca212fbded98
Simonni <szkiflay@gmal.com>
2018-12-06 14:27:00 -0500

    added mongoDB

--- server.js
+++ server.js
@@ -2,11 +2,23 @@
 const express = require('express')
 const cors = require('cors')
 const superagent = require('superagent')
+const mongoose = require('mongoose')
+const { Schema, model } = mongoose
 
 require('dotenv').config()
+
+const mongoURL = `mongodb://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@ds147872.mlab.com:47872/md301`
+
+mongoose.connect(mongoURL)
+const db = mongoose.connection
+
+db.on('error', console.error.bind(console, 'connection error:'))
+db.once('open', () => {
+  console.log('DB connection open!')
+})
+
 const port = process.env.port ||3000
 const app = express()
-
 
 app.use(cors())
 
@@ -15,12 +27,25 @@ app.get('/', (req, res) =>
 
 app.get('/location', (req, res)=>{
   const url = `https://maps.googleapis.com/maps/api/geocode/json?address=${req.query.address}&key=${process.env.GOOGLE_API_KEY}`
-  superagent.get(url)
-    .then(result => {
-      res.send(new Location(result))
-    })
-
-    .catch(err => res.send(err))
+  Location.findOne({ address: req.query.address}, (err, addr) => {
+    if(addr) {
+      console.log('address found')
+      res.send(addr)
+    } else {
+      superagent.get(url)
+        .then(result => {
+          const newLocation = new location({address:req.query.address,
+            lng: result.body.results[0].geometry.location.lng,
+            lat: result.body.results[0].geometry.location.lat 
+          })
+          newLocation.save()
+          console.log('created new address')
+          res.send(newLocation)
+        })
+    }
+  })
+  
+    .catch(err => res.send(err)) 
 })
 
 app.get('*', (req, res)=>
@@ -34,3 +59,11 @@ const Location = function(loc){
   this.lat = loc.body.results[0].geometry.location.lat
   this.lng = loc.body.results[0].geometry.location.lng
 }
+
+const LocationSchema = new Schema({
+  address: String,
+  lat: Number,
+  lng: Number
+})
+
+const Location = model('Location', LocationSchema)
